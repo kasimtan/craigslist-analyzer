d5b00ee8622dfe4e8308130f94af46be2bb315ae
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-10-29 21:55:12 +0000

    Eliminate the locations bug.

--- trunk/src/com/test/CrawlTest.java
+++ trunk/src/com/test/CrawlTest.java
@@ -4,10 +4,15 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import com.crawl.control.Crawler;
 
+/**
+ * 
+ * @author mschimpf
+ *
+ */
 public class CrawlTest {
 	   @Test   
 	   public void test_createCrawler() {      
-		   System.out.println("Test if pricePerMonth returns Euro...") ;      
+		   System.out.println("Create Crawler class...") ;      
 		   Crawler c = new Crawler() ;      
 		   assertTrue(c != null) ;   
 	   }   	   
