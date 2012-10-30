eed7d938bebeca2761dafbf3fda0c3e96a49d2ce
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-10-30 00:51:06 +0000

    Add logger statements.

--- src/com/craigslist/starter/StartMain.java
+++ src/com/craigslist/starter/StartMain.java
@@ -2,6 +2,10 @@ package com.craigslist.starter;
 
 import java.util.Collection;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.crawl.control.Crawler;
 import com.crawl.model.CraigslistCategoryEnum;
 import com.crawl.model.CrawlResultPackage;
@@ -12,19 +16,31 @@ import com.crawl.model.CrawlResultPackage;
  * 
  */
 public class StartMain {
-
+	// Define a static logger variable so that it references the
+	// Logger instance named "MyApp".
+	static Logger logger = Logger.getLogger(StartMain.class);
+	
 	/**
 	 * Analyzing main function
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// Set up a simple configuration that logs on the console.
+	    BasicConfigurator.configure();
+	    logger.info("Entering application.");
+	    
+	    // BasicConfigurator replaced with PropertyConfigurator.
+	    PropertyConfigurator.configure("log4j.configuration");
+
 		Crawler aCrawl=new Crawler();
 		
-		Collection<CrawlResultPackage> aResultColl=aCrawl.crawlWebPages(CraigslistCategoryEnum.FOR_SALE__COMPUTER, "", 200 /*Max Offers*/);
+		Collection<CrawlResultPackage> aResultColl=aCrawl.crawlWebPages(CraigslistCategoryEnum.FOR_SALE__COMPUTER, "", 10 /*Max Offers*/);
 		
 		for (CrawlResultPackage myPackage:aResultColl){
-			System.out.println("myPackage Price="+myPackage.getPriceOfItem()+" Locations="+myPackage.getLocationsAsString());
+			logger.info("myPackage Price="+myPackage.getPriceOfItem()+" Locations="+myPackage.getLocationsAsString());
 		}
+		
+		logger.info("Ending application.");
 	}
 }
--- src/somePackage/SomeBean.java
+++ src/somePackage/SomeBean.java
@@ -1,12 +1,18 @@
 package somePackage;
 
 import javax.faces.bean.*;
+import org.apache.log4j.Logger;
+
+import com.test.CrawlTest;
+
 import java.io.Serializable;
 
 @ManagedBean
 @SessionScoped
 public class SomeBean implements Serializable {
   private static final long serialVersionUID = 1L;
+  
+  static Logger logger = Logger.getLogger(SomeBean.class);
 	
   private String someProperty = "";
 
