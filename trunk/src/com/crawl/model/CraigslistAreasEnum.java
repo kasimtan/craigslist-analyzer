616802c8fdf4bceeea1c95e2e02a3b546a950a5e
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-11-01 20:14:36 +0000

    Code the locations search.

--- trunk/src/com/crawl/model/CraigslistAreasEnum.java
+++ trunk/src/com/crawl/model/CraigslistAreasEnum.java
@@ -11,15 +11,19 @@ import com.crawl.control.Crawler;
  */
 public enum CraigslistAreasEnum {
 	// The main area
-	MAIN_AREA_SF_BAY_AREA("sfbay"),
+	//MAIN_AREA_SF_BAY_AREA("sfbay"),
+	MAIN_AREA_SF_BAY_AREA(""),
 	
 	// Sub areas of sfbay
-	SAN_FRANCISCO("sfc"),
-	SOUTH_BAY("sby"),
-	EAST_BAY("eby"),
-	PENINSULA("pen"),
-	NORTH_BAY("nby"),
-	SANTA_CRUZ("scz");
+	SAN_FRANCISCO("/sfc"),
+	SOUTH_BAY("/sby"),
+	EAST_BAY("/eby"),
+	PENINSULA("/pen"),
+	NORTH_BAY("/nby"),
+	SANTA_CRUZ("/scz"),
+	
+	// Only for internal use
+	URL_CONST_AREA_SF_BAY_AREA("sfbay");
 	
 	private String code;  
 	
