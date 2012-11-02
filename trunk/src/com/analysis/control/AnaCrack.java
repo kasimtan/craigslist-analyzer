fc8b112639e5e361cb5110f962cc75673352a23c
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-11-02 21:38:00 +0000

    Create statistic methods.

--- trunk/src/com/analysis/control/AnaCrack.java
+++ trunk/src/com/analysis/control/AnaCrack.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 
 import org.apache.log4j.Logger;
 
-import com.crawl.control.Crawler;
 import com.crawl.model.CraigslistAlgorithmEnum;
 import com.crawl.model.CrawlResultPackage;
 
@@ -18,12 +17,13 @@ public class AnaCrack {
     static Logger logger = Logger.getLogger(AnaCrack.class);
 
     private Collection<CrawlResultPackage> crawlCollection;
+    private int offers;
     private int average;
     private int min;
     private int max;
-    private int median;
+    private double median;
     private int mode;
-    private int standardDeviation;
+    private double standardDeviation;
     
 	/**
 	 * 
@@ -35,14 +35,191 @@ public class AnaCrack {
 	 */
 	public Collection<CrawlResultPackage> getBestOffers(Collection<CrawlResultPackage> inputCrawlerResultColl, int inputInputHowMuch, CraigslistAlgorithmEnum inputCraigslistAlgorithmEnum, int inputLowerControlLimit, int inputHigherControlLimit){
 	    this.setCrawlCollection(inputCrawlerResultColl);
+	    this.setOffers(inputCrawlerResultColl.size());
 	    
-        for (CrawlResultPackage myPackage : inputCrawlerResultColl) {
-            logger.info("CrawlResultPackage\n" + myPackage.toString());
-        }
+	    this.createAverage();
+	    this.searchForMinPrice();
+	    this.searchForMaxPrice();
+	    this.searchForMedian();
+	    this.searchForMode();
+	    this.searchForStandardDeviation();
 	    
 		return new ArrayList<CrawlResultPackage>();
-		
 	}
+	
+	private void searchForStandardDeviation(){   
+        double[] doubleArray=new double[this.getOffers()];
+        int a=0;
+        
+        for (CrawlResultPackage myPackage : this.getCrawlCollection()) {
+            logger.debug("CrawlResultPackage\n" + myPackage.toString());
+            doubleArray[a]=myPackage.getPriceOfItem();
+            a++;
+        }
+	      
+        double sum = 0;
+
+        // Taking the average to numbers
+        for(int i =0; i< doubleArray.length; i++) {
+         sum = sum + doubleArray[i];
+        }
+
+        double mean = sum/doubleArray.length;
+
+        logger.debug(mean);
+
+        double[] deviations = new double[10];
+
+        // Taking the deviation of mean from each numbers
+        for(int i = 0; i < deviations.length; i++) {
+         deviations[i] = doubleArray[i] - mean ; 
+         logger.debug(""+deviations[i]);   
+        }
+
+        double[] squares = new double[10];
+
+        // getting the squares of deviations
+        for(int i =0; i< squares.length; i++) {
+         squares[i] = deviations[i] * deviations[i];
+         logger.debug(""+squares[i]);
+        }
+
+        sum = 0;
+
+        // adding all the squares
+        for(int i =0; i< squares.length; i++) {
+         sum = sum + squares[i];
+        }
+
+        logger.debug(sum);
+
+        // dividing the numbers by one less than total numbers
+        double result = sum / (doubleArray.length - 1);
+
+        double standardDeviation = Math.sqrt(result);
+                
+        this.setStandardDeviation(standardDeviation);
+	    logger.info("Standard Deviation="+this.getStandardDeviation()+" offers="+this.getOffers());
+	}  
+
+	
+	private void searchForMode() {
+        int[] intArray=new int[this.getOffers()];
+        int a=0;
+        
+        for (CrawlResultPackage myPackage : this.getCrawlCollection()) {
+            logger.debug("CrawlResultPackage\n" + myPackage.toString());
+            intArray[a]=myPackage.getPriceOfItem();
+            a++;
+        }
+	    
+	    
+	    int maxValue=0, maxCount=0;
+
+	    for (int i = 0; i < intArray.length; ++i) {
+	        int count = 0;
+	        for (int j = 0; j < intArray.length; ++j) {
+	            if (intArray[j] == intArray[i]) ++count;
+	        }
+	        if (count > maxCount) {
+	            maxCount = count;
+	            maxValue = intArray[i];
+	        }
+	    }
+
+	    this.setMode(maxValue);
+	    logger.info("Mode="+this.getMode()+" offers="+this.getOffers());
+	}	
+	
+	private void searchForMedian() {
+	    int[] intArray=new int[this.getOffers()];
+	    int i=0;
+	    
+        for (CrawlResultPackage myPackage : this.getCrawlCollection()) {
+            logger.debug("CrawlResultPackage\n" + myPackage.toString());
+            intArray[i]=myPackage.getPriceOfItem();
+            i++;
+        }
+	    	    
+	    int middle = intArray.length/2;  // subscript of middle element
+	    
+	    if (intArray.length%2 == 1) {
+	        // Odd number of elements -- return the middle one.
+	        double retValue=intArray[middle];
+	        this.setMedian(retValue);
+	        logger.info("Median="+this.getMedian()+" offers="+this.getOffers());
+	        return;
+	    } else {
+	       // Even number -- return average of middle two
+	       // Must cast the numbers to double before dividing.
+	       double retValue=(intArray[middle-1] + intArray[middle]) / 2.0;
+	       this.setMedian(retValue);
+	       logger.info("Median="+this.getMedian()+" offers="+this.getOffers());
+	       return;
+	    }
+	}
+
+	private void searchForMinPrice(){
+        int aMinPrice=1000000;
+        
+        for (CrawlResultPackage myPackage : this.getCrawlCollection()) {
+            logger.debug("CrawlResultPackage\n" + myPackage.toString());
+         
+            if (myPackage.getPriceOfItem()<aMinPrice){
+                aMinPrice=myPackage.getPriceOfItem();
+                logger.debug("aMinPrice="+aMinPrice);
+            }            
+        }
+        
+        this.setMin(aMinPrice);
+        
+        logger.info("aMinPrice="+this.getMin()+" offers="+this.getOffers());        
+	    
+	}
+	
+    private void searchForMaxPrice(){
+        int aMaxPrice=0;
+        
+        for (CrawlResultPackage myPackage : this.getCrawlCollection()) {
+            logger.debug("CrawlResultPackage\n" + myPackage.toString());
+         
+            if (myPackage.getPriceOfItem()>aMaxPrice){
+                aMaxPrice=myPackage.getPriceOfItem();
+                logger.debug("aMaxPrice="+aMaxPrice);
+            }            
+        }
+        
+        this.setMax(aMaxPrice);
+        
+        logger.info("FINAL aMaxPrice="+this.getMax()+" offers="+this.getOffers());                
+    }
+	
+	
+	private void createAverage(){
+        int aPrice=0;
+        
+        for (CrawlResultPackage myPackage : this.getCrawlCollection()) {
+            logger.debug("CrawlResultPackage\n" + myPackage.toString());
+         
+            aPrice=aPrice+myPackage.getPriceOfItem();
+        }
+        
+        this.setAverage((aPrice/this.getOffers()));
+        
+        logger.info("average price="+this.getAverage()+" offers="+this.getOffers());	    
+	}
+	
+	private void analyzeAndDoStaistics(){
+	    
+	}
+	
+    public int getOffers() {
+        return offers;
+    }
+
+    private void setOffers(int offers) {
+        this.offers = offers;
+    }	
 	
 	public Collection<CrawlResultPackage> getCrawlCollection() {
         return crawlCollection;
@@ -71,7 +248,7 @@ public class AnaCrack {
         this.max = max;
     }
 
-    private void setMedian(int median) {
+    private void setMedian(double median) {
         this.median = median;
     }
 
@@ -79,7 +256,7 @@ public class AnaCrack {
         this.mode = mode;
     }
 
-    private void setStandardDeviation(int standardDeviation) {
+    private void setStandardDeviation(double standardDeviation) {
         this.standardDeviation = standardDeviation;
     }
 
@@ -94,13 +271,13 @@ public class AnaCrack {
 	 * 
 	 */	
 	public int getMax(){
-		return this.mode;
+		return this.max;
 	}
 	
 	/**
 	 * 
 	 */	
-	public int getMedian(){
+	public double getMedian(){
 		return this.median;
 	}
 
