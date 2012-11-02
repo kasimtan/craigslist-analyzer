5ee159023bba8a8fb199707cd0a1b4f49e835cf9
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-11-02 01:55:46 +0000

    Add new test cases.

--- src/com/test/CrawlTest.java
+++ src/com/test/CrawlTest.java
@@ -1,10 +1,15 @@
 package com.test;
 
-import org.apache.log4j.Logger;
-
 import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
 import org.junit.Test;
+
 import com.crawl.control.Crawler;
+import com.crawl.model.CraigslistAreasEnum;
+import com.crawl.model.CraigslistCategoryEnum;
 import com.crawl.model.CrawlResultPackage;
 
 /**
@@ -17,8 +22,121 @@ public class CrawlTest {
 
 	@Test
 	public void test_createCrawler() {
-		System.out.println("Create Crawler class...");
+		logger.info("Create Crawler class...");
 		Crawler c = new Crawler();
 		assertTrue(c != null);
 	}
+	
+    @Test
+    public void test_callGetWebPagesMethod() {
+        logger.info("...");
+        
+        Crawler c = new Crawler();
+        
+        Collection<CrawlResultPackage> aCrawlResultColl=c.crawlWebPages(CraigslistCategoryEnum.FOR_SALE__COMPUTER, CraigslistAreasEnum.PENINSULA, "Apple", 100);
+        assertTrue(null == null);
+        assertTrue(aCrawlResultColl.size()!=0);
+    }	
+    
+    @Test
+    public void test_2() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_3() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_4() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_5() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_6() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_7() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_8() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_9() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_10() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_11() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_12() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_13() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_14() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_15() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_16() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_17() {
+        logger.info("...");
+        assertTrue(null == null);
+    }
+    
+    @Test
+    public void test_18() {
+        logger.info("...");
+        assertTrue(null == null);
+    }    
 }
