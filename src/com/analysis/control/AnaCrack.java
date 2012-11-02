503fd8617a0bde54dd819a3500263521041f5c90
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-11-02 02:11:15 +0000

    Add getter and setter to AnaCrack

--- src/com/analysis/control/AnaCrack.java
+++ src/com/analysis/control/AnaCrack.java
@@ -3,6 +3,9 @@ package com.analysis.control;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.log4j.Logger;
+
+import com.crawl.control.Crawler;
 import com.crawl.model.CraigslistAlgorithmEnum;
 import com.crawl.model.CrawlResultPackage;
 
@@ -12,6 +15,16 @@ import com.crawl.model.CrawlResultPackage;
  *
  */
 public class AnaCrack {
+    static Logger logger = Logger.getLogger(AnaCrack.class);
+
+    private Collection<CrawlResultPackage> crawlCollection;
+    private int average;
+    private int min;
+    private int max;
+    private int median;
+    private int mode;
+    private int standardDeviation;
+    
 	/**
 	 * 
 	 * @param inputInputHowMuch Give me the 10 best offers
@@ -21,48 +34,87 @@ public class AnaCrack {
 	 * @return
 	 */
 	public Collection<CrawlResultPackage> getBestOffers(Collection<CrawlResultPackage> inputCrawlerResultColl, int inputInputHowMuch, CraigslistAlgorithmEnum inputCraigslistAlgorithmEnum, int inputLowerControlLimit, int inputHigherControlLimit){
+	    this.setCrawlCollection(inputCrawlerResultColl);
+	    
+        for (CrawlResultPackage myPackage : inputCrawlerResultColl) {
+            logger.info("CrawlResultPackage\n" + myPackage.toString());
+        }
+	    
 		return new ArrayList<CrawlResultPackage>();
+		
 	}
 	
-	/**
+	public Collection<CrawlResultPackage> getCrawlCollection() {
+        return crawlCollection;
+    }
+
+    private void setCrawlCollection(Collection<CrawlResultPackage> crawlCollection) {
+        this.crawlCollection = crawlCollection;
+    }
+
+    /**
 	 * 
 	 */
-	public int getAverage(){
-		return -1;
+	private int getAverage(){
+		return this.average;
 	}
 
-	/**
+	private void setAverage(int average) {
+        this.average = average;
+    }
+
+    private void setMin(int min) {
+        this.min = min;
+    }
+
+    private void setMax(int max) {
+        this.max = max;
+    }
+
+    private void setMedian(int median) {
+        this.median = median;
+    }
+
+    private void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    private void setStandardDeviation(int standardDeviation) {
+        this.standardDeviation = standardDeviation;
+    }
+
+    /**
 	 * 
 	 */
 	public int getMin(){
-		return -1;
+		return this.min;
 	}
 	
 	/**
 	 * 
 	 */	
 	public int getMax(){
-		return -1;
+		return this.mode;
 	}
 	
 	/**
 	 * 
 	 */	
 	public int getMedian(){
-		return -1;
+		return this.median;
 	}
 
 	/**
 	 * 
 	 */
 	public int getMode(){
-		return -1;
+		return this.mode;
 	}
 	
 	/**
 	 * 
 	 */	
 	public double getStandardDeviation(){
-		return 0.0;
+		return this.standardDeviation;
 	}
 }
