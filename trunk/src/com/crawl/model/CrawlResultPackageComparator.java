8e2c7da918d8f226ef3ed2db1a066bf902ffee2d
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-11-06 21:39:20 +0000

    Add Comparator Interface and try to test it.

---
+++ trunk/src/com/crawl/model/CrawlResultPackageComparator.java
@@ -0,0 +1,18 @@
+package com.crawl.model;
+
+import java.util.Comparator;
+
+/**
+ * Sort Algorithmen.
+ * @author mschimpf
+ *
+ */
+public class CrawlResultPackageComparator implements Comparator<CrawlResultPackage>{
+    @Override public int compare(CrawlResultPackage inputOne, CrawlResultPackage inputTwo){
+        if (inputOne.getPriceOfItem()==inputTwo.getPriceOfItem()){
+            return 0;
+        } else {
+            return inputOne.getPriceOfItem()-inputTwo.getPriceOfItem();
+        }
+    }
+}
