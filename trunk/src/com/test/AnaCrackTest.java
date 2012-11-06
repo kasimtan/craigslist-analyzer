2cbf42ff63f9769e0de85d25faa8f332eec9c886
schimpf.michael.MS@gmail.com <schimpf.michael.MS@gmail.com@3005fb16-4a16-cda5-7975-228cb9b78bd9>
2012-11-06 21:05:00 +0000

    Add some test cases.

--- trunk/src/com/test/AnaCrackTest.java
+++ trunk/src/com/test/AnaCrackTest.java
@@ -2,10 +2,15 @@ package com.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import com.analysis.control.AnaCrack;
+import com.crawl.model.CraigslistAlgorithmEnum;
+import com.crawl.model.CrawlResultPackage;
 
 /**
  * 
@@ -23,14 +28,32 @@ public class AnaCrackTest {
 	}
 	
     @Test
-    public void test_2() {
-        logger.debug("...");
+    public void test_illegalArgumentException() {
+        logger.info("Catch expected IllegalArgumentException");
+        logger.info("Create AnaCrack class and give over nothing null and -1 ...");
+        AnaCrack c = new AnaCrack();
+        
+        try{
+            Collection<CrawlResultPackage> aColl=c.getBestOffers(null, -1, null, -1, -1);
+        } catch (IllegalArgumentException e){
+            logger.info("Catch expected IllegalArgumentException exception");
+            logger.info(e);
+        }
+ 
         assertTrue(null == null);
     }
     
     @Test
-    public void test_3() {
-        logger.debug("...");
+    public void test_collection() {
+        logger.info("Catch expected IllegalArgumentException");
+        
+        AnaCrack c = new AnaCrack();
+        
+        // ToDo try something lower then 10 Packages and you will receive an Exception
+        Collection<CrawlResultPackage> aColl=c.getBestOffers(AnaCrackTest.generateFakeColl(1, 10, 1), 10, CraigslistAlgorithmEnum.BEST, 1, 10);
+
+        logger.info(""+c.toString());
+        
         assertTrue(null == null);
     }
     
@@ -123,4 +146,34 @@ public class AnaCrackTest {
         logger.debug("...");
         assertTrue(null == null);
     } 
+    
+    /**
+     * Utility method to produce test objects.
+     * @param inputHowMuchPackages
+     * @param inputPrice
+     * @return
+     */
+    private static Collection<CrawlResultPackage> generateFakeColl(
+            int inputFromPrice,
+            int inputToPrice,
+            int inputSteps){
+        Collection<CrawlResultPackage> aRetColl=new ArrayList<CrawlResultPackage>();
+        
+        for (int i=inputFromPrice;i<inputToPrice;i=i+inputSteps){
+            CrawlResultPackage aPack=new CrawlResultPackage();
+            aPack.setItem("PC Laptop &amp; Macbook Repairs System Software &amp; Data Recovery Start from");
+            aPack.setLine("<a href=\"http://sfbay.craigslist.org/sfc/syd/3377230486.html\">$29 - PC Laptop &amp; Macbook Repairs System Software &amp; Data Recovery Start from</a>");
+            Collection<String> aStringColl=new ArrayList<String>();
+            aStringColl.add("downtown");
+            aStringColl.add("civic");
+            aStringColl.add("van ness");
+            aPack.setLocations(aStringColl);
+            aPack.setPriceOfItem(i);
+            aPack.setUrl("http://sfbay.craigslist.org/sfc/syd/3377230486.html");  
+            logger.debug(aPack.toString());
+            aRetColl.add(aPack);
+        }
+        
+        return aRetColl;
+    }
 }
